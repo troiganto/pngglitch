5299a45d0a00b1a00bae3658ea0850bba4d8050b
Nico Madysa <nico.madysa@tu-dresden.de>
2019-07-15 23:16:53 +0200

    Make project setuptools-compatible.

---
+++ pngglitch/__main__.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""The command-line script that is part of `pngglitch`."""
+
+import os
+import random
+import argparse
+
+from pngglitch import GlitchedPNGFile
+
+
+def parse_args():
+    """Interface to the command-line."""
+
+    def insert_index_into_filename(filename):
+        """Turns "a.png" into "a.%d.png"."""
+        pre, dot, suff = filename.rpartition('.')
+        return pre + '.%d.' + suff
+
+    def make_scrambled_filename(infile):
+        """Returns a filename like "c40ac12baa3be95c.png"."""
+        outfile = infile
+        while os.path.isfile(outfile):
+            randint = random.randint(0, 16**16 - 1)
+            outfile = format(randint, '016x') + '.png'
+        return outfile
+
+    # Parse the incoming parameters.
+    parser = argparse.ArgumentParser(
+        description='Create glitch effects in PNG files '
+        'by actually corrupting them.', )
+    parser.add_argument(
+        '--outfile',
+        '-o',
+        dest='outfile',
+        action='store',
+        type=str,
+        help='Naming pattern for the output files. '
+        'Defaults to "<infile>.%%d.png" if --num is '
+        'specified and to "<infile>.corrupt.png" '
+        'otherwise.',
+    )
+    parser.add_argument(
+        '--num',
+        '-N',
+        dest='number',
+        action='store',
+        default=1,
+        metavar='N',
+        type=int,
+        help='Number of output files. If not specified, '
+        'one output file will be created.',
+    )
+    parser.add_argument(
+        '-R',
+        dest='randomize',
+        action='store_true',
+        default=False,
+        help=format(random.randint(0, 16**16 - 1), '016x'),
+    )
+    parser.add_argument(
+        '--amount',
+        '-a',
+        dest='amount',
+        metavar='INT',
+        action='store',
+        type=int,
+        default=100,
+        help='Describes how many byte should be affected '
+        'in total. Defaults to 100.',
+    )
+    parser.add_argument(
+        '--mean',
+        '-m',
+        dest='mean',
+        metavar='INT',
+        action='store',
+        type=int,
+        default=20,
+        help='Mean glitch size in bytes. Defaults to 20.',
+    )
+    parser.add_argument(
+        '--deviation',
+        '-d',
+        dest='dev',
+        metavar='INT',
+        action='store',
+        type=int,
+        default=5,
+        help='Standard deviation of glitch size in bytes. '
+        'Defaults to 5.',
+    )
+    parser.add_argument(
+        'infile',
+        metavar='INFILE',
+        action='store',
+        type=str,
+        help='PNG file to be corrupted.',
+    )
+    args = parser.parse_args()
+
+    # Sanitize output filename.
+    if args.outfile is None:
+        if args.number > 1:
+            # multi-file output
+            args.outfile = insert_index_into_filename(args.infile)
+        elif args.randomize:
+            # single-file output, random name.
+            args.outfile = make_scrambled_filename(args.infile)
+        else:
+            # single-file output, normal name.
+            args.outfile = args.infile.rpartition('.')[0] + '.corrupt.png'
+    elif args.number > 1:
+        # catch ill-formatted multifile patterns
+        try:
+            args.outfile % 1
+        except TypeError:
+            args.outfile = insert_index_into_filename(args.outfile)
+    else:
+        # if single-file output name is a pattern, insert 1.
+        try:
+            args.outfile = args.outfile % 1
+        except TypeError:
+            pass
+    return args
+
+
+def main():
+    """The main function."""
+    args = parse_args()
+    infile = GlitchedPNGFile(args.infile)
+    outfiles = infile.glitch_file(
+        copies=args.number,
+        glitch_amount=args.amount,
+        glitch_size=args.mean,
+        glitch_dev=args.dev,
+    )
+    if args.number > 1:
+        for i, outfile in enumerate(outfiles):
+            outfile.write(args.outfile % i)
+    else:
+        outfiles.next().write(args.outfile)
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright 2019 Nico Madysa
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+
+long_description = """\
+This script offers a class GlitchedPNGFile that enables the user
+to partially decompress a PNG file and deliberately put errors into
+it's bytestream.
+
+It can also be called from the command line to corrupt PNG files individually
+or in bulk.
+"""
+
+setup(
+    name='pngglitch',
+    version='1.0.1',
+    python_requires='>=2.7',
+    packages=['pngglitch'],
+    entry_points={
+        'console_scripts': [
+            'pngglitch = pngglitch.__main__:main',
+            ]
+        },
+    zip_safe=True,
+    author='Nico Madysa',
+    author_email='uebertreiber@gmx.de',
+    description='Corrupt PNG files without breaking their checksums',
+    long_description=long_description,
+    license='Apache License, Version 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Artistic Software',
+        ],
+    keywords='png glitch art',
+    url='https://github.com/troiganto/pngglitch',
+)
